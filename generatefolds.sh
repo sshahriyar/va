b0a93b499c94ecfe218aa600613f66c727569a4a
Shary M <syed.shariyar@gmail.com>
2017-11-28 16:04:52 -0500

    Scripts to execute code

---
+++ generatefolds.sh
@@ -0,0 +1,23 @@
+###
+### Use this file as an example to generate training and test files ##  for n folds
+### and to execute OAA-NBC on the generated data. 
+###
+ echo "*** Generating files for 10 folds"
+ ## parameters:
+# 1. "f" for files
+# 2. "dataset/mds_nom.arff" input file
+# 3. "10" Number of folds
+# 4. "15" total classes in the dataset (Excluding others which is used by OAA-NBC only)
+# 5. "f" for false and "t" for true foe dirichlet distribution based test set
+
+
+
+java -jar  lib/oaanbc.jar  "f" "dataset/mds_nom.arff" "10"  "15" "dataset_mds"  "f"
+
+
+
+echo "****************Generate files uisng 10 folds but generate the test set using Dirichlet distribution ***************"
+
+
+java -jar  lib/oaanbc.jar  "f" "dataset/mds_nom.arff" "10"  "15" "dataset_dirich_mds"  "t"
+
--- measures.sh
+++ measures.sh
@@ -3,11 +3,28 @@
 ### sensitivity, specificity, PCCC and CSMF accuracy from
 ### the output of the algorithms generated using R
 ###
+
+ ## parameters:
+# 1. "m" for measurements
+# 2. "dataset/mds_nom.arff" input file
+# 3. "10" Number of folds
+# 4. "15" total classes in the dataset (Excluding others which is used by OAA-NBC only)
+
 for algo in "naivebayes" "tariff" "interva" "insilico"
 do
 echo $algo
-java -jar  oaanbc.jar  "m" "/home/shary/sharywork/ryerson/experiment_data/finaldata/dataset_dirich_matlab/output_$algo" "10"  "15" 
+java -jar  oaanbc.jar  "m" "dataset/dirichlet/dataset_dirich_matlab/output_$algo" "10"  "15" 
 
-#java -jar  oaanbc.jar  "m" "/home/shary/sharywork/ryerson/experiment_data/finaldata/dataset_dirich_agincourt/output_$algo" "100"  "16" 
+
+
+done
+
+
+for algo in "naivebayes" "tariff" "interva" "insilico"
+do
+echo $algo
+java -jar  oaanbc.jar  "m" "dataset/regular/dataset_matlab/output_$algo" "10"  "15" 
+
+
 
 done
